7239b555258542662337a3097bca096ceae78c0c
itechbear <itechbear@gmail.com>
2014-07-27 22:39:34 +0800

    format hints

--- src/com/github/itechbear/macroexpansion/formatter/Formatter.java
+++ src/com/github/itechbear/macroexpansion/formatter/Formatter.java
@@ -1,9 +1,16 @@
 package com.github.itechbear.macroexpansion.formatter;
 
+import com.intellij.codeInsight.actions.ReformatCodeProcessor;
 import com.intellij.lang.Language;
+import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.RunResult;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.command.WriteCommandAction;
 import com.intellij.openapi.editor.impl.DocumentImpl;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
@@ -22,45 +29,20 @@ public class Formatter {
         return format(project, language, text);
     }
 
-    public static String format(Project project, Language language, String text) {
-        DocumentImpl document2 = new DocumentImpl(text, true, true);
-        VirtualFile virtualFile = FileDocumentManager.getInstance().getFile(document2);
-//        PsiFile psiFile = PsiDocumentManager.getInstance(project).getCachedPsiFile(document);
-//        if (psiFile == null) {
-//            return text;
-//        }
-        PsiFile psiFile = PsiFileFactoryImpl.getInstance(project).createFileFromText("virtual", language, text);
-        DocumentImpl document = (DocumentImpl) FileDocumentManager.getInstance().getDocument(psiFile.getVirtualFile());
-        Field field = null;
-        try {
-            field = DocumentImpl.class.getDeclaredField("myAssertThreading");
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
+    public static String format(final Project project, Language language, String text) {
+        final PsiFile psiFile = PsiFileFactoryImpl.getInstance(project).createFileFromText("virtual", language, text);
+        WriteCommandAction.Simple<String> command = new WriteCommandAction.Simple<String>(project, psiFile) {
+
+            @Override
+            protected void run() throws Throwable {
+                CodeStyleManager.getInstance(project).reformat(psiFile, false);
+            }
+        };
+        command.execute();
+        String formatted = psiFile.getText();
+        if (formatted == null) {
             return text;
         }
-        if (field == null) {
-            return text;
-        }
-        field.setAccessible(true);
-        try {
-            field.set(document, false);
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return text;
-        }
-//        CodeStyleSettings codeStyleSettings = ProjectCodeStyleSettingsManager.getSettings(project);
-//        FormattingModelBuilder formattingModel = LanguageFormatting.INSTANCE.forContext(psiFile);
-//        if (formattingModel == null) {
-//            return text;
-//        }
-//        FormattingModel model = formattingModel.createModel(psiFile, codeStyleSettings);
-//        model.commitChanges();
-//        return psiFile.getText();
-//        ReformatCodeProcessor reformatCodeProcessor = new ReformatCodeProcessor(project, psiFile, psiFile.getTextRange(), false);
-//        FutureTask<Boolean> task = reformatCodeProcessor.preprocessFile(psiFile, false);
-//        CommandProcessor commandProcessor = CommandProcessor.getInstance();
-//        task.run();
-        CodeStyleManager.getInstance(project).reformat(psiFile, false);
-        return psiFile.getText();
+        return formatted.replace(" ", "&nbsp;");
     }
 }
